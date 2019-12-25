9807963386a8f5acf57b7d849b7b30b45113cec6
Michael Greenberg <mgreenbe@redhat.com>
2019-12-25 21:23:08 +0200

    fixed typo

--- retag_registry.sh
+++ retag_registry.sh
@@ -9,6 +9,6 @@ for image in $(docker images | awk '!/NAME/{print $1":"$2}');do
 	if [[ ${image} =~ / ]];then
 		docker tag ${image} $(echo $image | sed "s/[^/]*/$1/")
 	else
-		docker tage ${image} $(echo $1/$image)
+		docker tag ${image} $(echo $1/$image)
 	fi
 done

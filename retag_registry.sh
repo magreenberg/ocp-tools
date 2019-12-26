dd14f5fb4483fbbff1808be43d947fa6cb841041
Michael Greenberg <mgreenbe@redhat.com>
2019-12-26 08:45:07 +0200

    added push option

--- retag_registry.sh
+++ retag_registry.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-if [ $# -ne 1 ];then
-	echo "Usage: $(basename $0) tag"
+PROGNAME=$(basename "$0")
+USAGE="Usage: ${PROGNAME} [-p] -t tag"
+unset PUSH
+unset TAG
+
+while getopts pt: c
+do
+	case $c in
+	p)	PUSH="true";;
+	t)	TAG=$OPTARG;;
+	\?)	echo "${USAGE}"
+		exit 2;;
+	esac
+done
+shift $((OPTIND -1))
+
+if [ -z "${TAG}" ];then
+	echo "missing tag"
+	echo "${USAGE}"
 	exit 2
 fi
 
 for image in $(docker images | awk '!/^REPOSITORY/{print $1":"$2}');do
 	if [[ ${image} =~ / ]];then
-		docker tag ${image} $(echo $image | sed "s/[^/]*/$1/")
+		NEWNAME=$(echo "$image" | sed "s/[^/]*/${TAG}/")
 	else
-		docker tag ${image} $(echo $1/$image)
+		NEWNAME="${TAG}/${image}"
+	fi
+	docker tag "${image}" "${NEWNAME}"
+	if [ -n "${PUSH}" ];then
+		docker push "${NEWNAME}"
 	fi
 done

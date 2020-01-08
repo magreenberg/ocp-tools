6d5eb0373aae4f9cae0111239a0c199e561b47be
Michael Greenberg <mgreenbe@redhat.com>
2020-01-08 10:22:05 +0200

    Enhanced functionality.

--- create_pv.sh
+++ create_pv.sh
@@ -2,21 +2,25 @@
 
 PROGNAME=$(basename "${0}")
 
-USAGE="Usage: ${PROGNAME} [-h] [-C cli] [-c storageclass] [-f from] [-t to] [-s size]"
+USAGE="Usage: ${PROGNAME} [-h] [-a accessmodes] [-C cli] [-c storageclass] [-f from] [-n name] [-s size] [-t to]"
 unset TO
+unset STORAGECLASS
 
 CLI="oc"
 FROM="1"
+unset NAME
 SIZE="1Gi"
-STORAGECLASS="manual"
+ACCESSMODE_ARG="ReadWriteOnce"
 
-while getopts C:c:f:ht:s: c
+while getopts a:C:c:f:hn:t:s: c
 do
 	case $c in
+	a) ACCESSMODE_ARG=${OPTARG};;
 	C) CLI=${OPTARG};;
-	c) STORAGECLASS=${OPTARG};;
+	c) STORAGECLASS="storageClassName: ${OPTARG}";;
 	f) FROM=${OPTARG};;
 	h) echo "${USAGE}";exit 0;;
+	n) NAME=${OPTARG};;
 	t) TO=${OPTARG};;
 	s) SIZE=${OPTARG};;
 	\?)	echo "${USAGE}"
@@ -25,8 +29,27 @@ do
 done
 shift $((OPTIND -1))
 
+if [ -n "${NAME}" ];then
+	if [ -n "${TO}" ];then
+		echo "${PROGNAME}: \"-n\" option cannot be used with \"-t\" option"
+		exit 2
+	else
+		FROM="1"
+		TO="1"
+	fi
+elif [ -z "${TO}" ];then
+	echo "\"-t\" option must be provided"
+	echo "${USAGE}"
+	exit 2
+fi
+
+ACCESSMODES="['${ACCESSMODE_ARG//,/\',\'}']"
 for pv in $(seq "${FROM}" "${TO}");do
-	pvname=$(printf "pv%03d" "${pv}")
+	if [ -n "${NAME}" ];then
+		pvname="${NAME}"
+	else
+		pvname=$(printf "pv%03d" "${pv}")
+	fi
 
 	${CLI} create -f - <<EOF
 apiVersion: "v1"
@@ -34,12 +57,10 @@ kind: PersistentVolume
 metadata:
   name: ${pvname}
 spec:
-  storageClassName: ${STORAGECLASS}
+  ${STORAGECLASS}
   capacity:
     storage: ${SIZE}
-  accessModes:
-  - ReadWriteOnce
-  - ReadWriteMany
+  accessModes: ${ACCESSMODES}
   persistentVolumeReclaimPolicy: Recycle
   hostPath:
 # may need selinux enforcement disabled
